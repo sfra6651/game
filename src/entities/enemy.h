9a52ced61c023c9798773ccb930a96fd1d9f927a
Shaun <sfra6651@gmail.com>
2026-03-15 00:50:48 +1000

    wip

---
+++ src/entities/enemy.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "raylib.h"
+
+#include "shared/components.h"
+#include "ecs/world.h"
+
+struct EnemyConfig {
+    Texture2D &texture;
+    Position pos = { (int)WINDOW_WIDTH/2, (int)WINDOW_HEIGHT/2 };
+    Size size = { 64, 64 };
+    Direction direction = { 0.f, 0.f };
+    Speed speed = { 0 };
+};
+
+inline Entity enemyFacotry(
+    World &world,
+    EnemyConfig config)
+{
+    Entity enemy = world.entities.create();
+    world.attach(enemy, world.positions, config.pos);
+    world.attach(enemy, world.renderables, Renderable{config.texture});
+    world.attach(enemy, world.sizes, config.size);
+    world.attach(enemy, world.directions, config.direction);
+    world.attach(enemy, world.speeds, config.speed);
+    return enemy;
+};
